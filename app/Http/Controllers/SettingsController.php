508876bf6a16ea5a556ced604762ba606f44dac0
Eveline <andreea.giosanu5@gmail.com>
2016-05-23 19:07:14 +0300

    Caught exception

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -15,33 +15,35 @@ use Redirect;
 class SettingsController extends BaseController
 {
 
-    public function show(){
-      $slideshare_error = Session::get('slideshare_error');
-      $username = Session::get('username');
-      $user_info = DB::select('SELECT username, email FROM users WHERE username=?',array($username));
-      $dir = getcwd();
-      $filename = $dir.'\img\profiles\\'.$user_info[0]->username.'.jpg';
-      if (file_exists($filename)) {
-        $user_info[0]->avatar= $user_info[0]->username;
-      } else {
-        $user_info[0]->avatar='default';
-      }
-      //Getting sources
-      $results = DB::select('SELECT source_name FROM accounts WHERE username=?',array($username));
-      $sources = array();
-      $select_values =  array('github', 'pocket', 'slideshare', 'vimeo');
-      foreach ($results as $result){
-        array_push($sources, $result->source_name);
-        if (($key = array_search($result->source_name, $select_values)) !== false) {
-            unset($select_values[$key]);
-        }
-      }
-      $user = Session::get('username');
-      // dd($sources);
-      return View::make('settings', ['sources' => $sources, 'user' => $user, 'select_values' => $select_values, 'slideshare_error' => $slideshare_error, 'user_info' => $user_info[0]]);
+  public function show(){
+    $slideshare_error = Session::get('slideshare_error');
+    $username = Session::get('username');
+    $user_info = DB::select('SELECT username, email FROM users WHERE username=?',array($username));
+    $dir = getcwd();
+    $filename = $dir.'\img\profiles\\'.$user_info[0]->username.'.jpg';
+    if (file_exists($filename)) {
+      $user_info[0]->avatar= $user_info[0]->username;
+    } else {
+      $user_info[0]->avatar='default';
     }
+      //Getting sources
+    $results = DB::select('SELECT source_name FROM accounts WHERE username=?',array($username));
+    $sources = array();
+    $select_values =  array('github', 'pocket', 'slideshare', 'vimeo');
+    foreach ($results as $result){
+      array_push($sources, $result->source_name);
+      if (($key = array_search($result->source_name, $select_values)) !== false) {
+        unset($select_values[$key]);
+      }
+    }
+    $user = Session::get('username');
+      // dd($sources);
+    return View::make('settings', ['sources' => $sources, 'user' => $user, 'select_values' => $select_values, 'slideshare_error' => $slideshare_error, 'user_info' => $user_info[0]]);
+  }
 
-    public function refresh(){
+  public function refresh(){
+    try{
+      
       //Refreshing Pocket content
       $pocket_controller = new PocketController();
       $pocket_controller->store();
@@ -49,11 +51,15 @@ class SettingsController extends BaseController
       $vimeo_controller =  new VimeoController;
       $vimeo_controller->store();
 
-      return Redirect::to('mycontent');
+    }catch(\Exception $e){
+      //If you don't have
+       return Redirect::to('mycontent');
     }
+    return Redirect::to('mycontent');
+  }
 
-    public function modify(){
+  public function modify(){
 
-    }
+  }
 
 }
