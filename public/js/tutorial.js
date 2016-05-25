d207c3e5819d2917e28131351ceb9fe530b2258a
anca9153 <adascalitei.anca@gmail.com>
2016-05-25 13:22:11 +0300

    Added text to our AWESOME tutorial :D :D :D

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
   //Tutorial text
   var tutorialArray = [
-    "<p>Welcome to RIX (Resource Interactive Explorer). Our application gives you the opportunity to gather all your tech content in one place. We support services like: Github, Pocket, Slideshare and Vimeo.</p><p>Follow the tutorial and get started by adding content from your favourite service</p>",
-    "<p>Step 2</p>",
-    "<p>Step 3</p>",
-    "<p>Step 4</p>",
-    "<p>Step 5</p>",
-    "<p>Step 6</p>",
-    "<p>Step 7</p>"
+    "<p>Welcome to RIX (Resource Interactive Explorer). Our application gives you the opportunity to gather all your tech content in one place. We support services like: Github, Pocket, Slideshare and Vimeo.</p><p>Follow the tutorial and get started by adding content from your favourite services.</p>",
+    "<p>You can add an account by clicking on the Add account button or by going to the Settings page and clicking on Connect account button.</p>",
+    "<p>You can then select the service with which you want to connect, in order to transfer your content from the selected account to RIX (Resource Interactive Explorer).</p>",
+    "<p>In order to find an article by title you can type into the search bar and all the articles that match will be shown. The list is updated as you type.</p>",
+    "<p>By clicking on Filters you will be able to choose the services from which you want to see your content.</p>",
+    "<p>By clicking on Refresh content on the Settings page, your content will be updated to the one on your added accounts.</p>",
+    "<p>On the Recommended content page you can see articles that are similar with your added content. This might take a while as we try our best to give you interesting content.</p>"
   ];
 
 
